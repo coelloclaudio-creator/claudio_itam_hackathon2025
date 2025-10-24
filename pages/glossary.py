7785d4c4efb7a28589d17f496f529e50884fc50a
coelloclaudio-creator <coelloclaudio@gmail.com>
2025-10-23 23:23:31 -0400

    Refactor glossary text for improved clarity
    Revised the glossary text for clarity and conciseness, improving the flow and readability of the project description.

--- pages/glossary.py
+++ pages/glossary.py
@@ -3,18 +3,18 @@ import streamlit as st
 st.header("Glossary")
 st.subheader('What was done in this project?')
 
-paragraph = """The first step was to use the player_season_stats from Statsbombpy to create a personalized data frame. What we did was take every row in this data frame, which represents one player’s statistical output over a full season, and try to find that same player’s previous season’s row. For example, we need to take Alvaro Fidalgo’s 2023/24 and 2022/23 seasons and add these to our new data frame. 
+paragraph = """
+The first step was to use the player_season_stats from Statsbombpy to create a personalized data frame. What we did was take every row in this data frame, which represents one player’s statistical output over a full season, and try to find that same player’s previous season’s row. For example, we need to take Alvaro Fidalgo’s 2023/24 and 2022/23 seasons and add these to our new data frame. 
+
 The columns are organized in this order:
-
 Fidalgo’s 2023/24 stats.
-
 Fidalgo’s 2022/23 stats.
-
-Very importantly, the difference between the first and second columns for each stat was also appended. In this project, we call the most recent of the two years “current”, and the former “previous.”
+Very importantly, the difference between the first and second columns for each stat was also appended.
+ In this project, we call the most recent of the two years “current”, and the former “previous.”
 
 We do this for every player and create a big database with each player's current and previous seasons.
 
-In a world where América signs players from every Liga MX team every year, we would have enough information to see which teams send over high-performing players to América, and which teams we should stop signing from. But this is not the case, in fact América has only signed a couple of players in the last 5 seasons, and some teams have not sent a player to América in over a decade. This is the gap our project is trying to bridge.
+In a world where América signs players from every Liga MX team every year, we would have enough information to see which teams send over high-performing players to América, and which teams we should stop signing from. But this is not the case, in fact América has only signed a couple of players in the last 5 seasons, and some teams have not sent a player to América in over a decade. This is the gap our project attempts to bridge.
 
 In order to do this, we started by using Statsbombpy’s team_season_stats and curated a selection of stats that tell us about the intention and the means a team employs when playing their games. These stats include:
 
@@ -28,12 +28,9 @@ In order to do this, we started by using Statsbombpy’s team_season_stats and c
  'Team_season_deep_progressions_pg'
  'Team_season_np_shots_pg'
 
-
 We also picked 4 key stats that explain teams’ performance during a season. 2 explain their offensive output, and 2 their defensive prowess. These are:
-
 'team_season_np_xg_pg', 'team_season_op_xg_pg', 
-                          'team_season_obv_pg', 'team_season_obv_conceded_pg
-                          
+ 'team_season_obv_pg', 'team_season_obv_conceded_pg
 
 We then conducted a Principal Component Analysis on these 2 groups of statistics separately. 
 
@@ -53,17 +50,17 @@ Sum of Total Output Current - Sum of Total Output Previous/ (Sum of Total Output
 With this, we can say for example:
 “The players that moved from Cluster 1 to Cluster 0 had a 20% dip in xG per 90 performance during their first season in their new cluster (often meaning new team).”
 
-The idea is to use this information to sign or scout players who currently play at teams belonging to clusters that transition well to América’s cluster. This goes one step further than a simple “this player has performed well, let’s sign him and see how he does in our scheme”. The clustering is an attempt to adjust to schematic differences and changes in team level.
+The idea is to use this information to sign or scout players who currently play at teams belonging to clusters that transition well to América’s cluster. This goes one step further than a simple “this player has performed well, let’s sign him and see how he does in our scheme”. The clustering is an attempt to adjust to schematic differences and team-level context. 
 
 However, this project has a lot of holes, and it could be improved with some changes and more time to work on it. There are 3 main ways this project could be improved with some adjustments to the database and outside help.
 
-First of all, Statsbomb does not partition Liga MX stats into apertura and clausura, which leaves a lot of holes and unknowns in this analysis. If we had access to an accurate database, there would be a lot more transitions counted and more granular data to work with.
+Statsbomb does not partition Liga MX stats into apertura and clausura, which leaves a lot of holes and unknowns in this analysis. If we had access to an accurate database, there would be a lot more transitions counted and more granular data to work with.
 
-Second, a data base including 10 or 15 years of league play would greatly increase the scope of the analysis, and we could even see how teams have changed their playing styles over the years.
+A database including 10 or 15 years of league play would greatly increase the scope of the analysis, and we could even see how teams have changed their playing styles over the years.
 
-Finally, access to more advanced scouting data, such as physical combine results, projected transfer fees, and even previous data from players that have played in other competitions would help better predict how a player might perform at América. 
+Access to more advanced scouting data, such as physical combine results, projected transfer fees, and even previous data from players that have played in other competitions would help better predict how a player might perform at América. 
 
-This is meant to be a tool to increase knowledge about signings in the transfer market, but a responsible scouting department would not use it as the end-all-be-all. A team needs to cross check the clustering data with their own “eye test” to see if the players our matrix predicts would perform well would actually fit their team needs.
+This is meant to be a tool to enhance scouting decisions, but a responsible scouting department would not use it as the end-all-be-all. A team needs to cross check the clustering data with their own “eye test” to see if the players our matrix identifies as good fits would actually meet the team’s current needs.
 """
 st.markdown(paragraph)
 
